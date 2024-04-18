3b056a1ac5dbd728e9dd8f43a251acaddc862a89
Klimczus <86327725+Klimczoczek@users.noreply.github.com>
2024-04-18 14:14:32 +0000

    zmiany w main

--- client/src/app/components/main/main.component.ts
+++ client/src/app/components/main/main.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
 
+  nickname = 'Anonymous#'+Math.floor(Math.random() * 10000);
+  gamejoin: boolean = false;
+  
+  options = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+
+
+  ifnicknone() {
+    if (this.nickname == '') {
+      this.nickname = 'Anonymous#'+Math.floor(Math.random() * 10000);
+    }
+  }
 }
